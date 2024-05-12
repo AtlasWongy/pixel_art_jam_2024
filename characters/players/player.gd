60344dbef4c103a0fede042f20c729f401575298
Shaw Kee <shawkee0511@gmail.com>
2024-05-12 23:35:05 +0800

    Update movement for rotation of pointer

--- characters/players/player.gd
+++ characters/players/player.gd
@@ -4,20 +4,25 @@ extends CharacterBody2D
 
 var rotation_direction = 0
 
-func get_input():
+func get_rotation_input(delta):
 	rotation_direction = Input.get_axis("left_arrow", "right_arrow")
-
-func _physics_process(delta):
-	get_input()
 	if rotation > 0.8:
-		rotation -= 0.025
-		return
+		if rotation_direction < 0:
+			rotation -= 0.025
+			print(rotation)
+			return
 	elif rotation < -0.8:
-		rotation += 0.025
-		return
+		if rotation_direction > 0:
+			rotation += 0.025
+			print(rotation)
+			return
 	else:
 		rotation += rotation_direction * rotation_speed * delta
-
+	
+		
+func _physics_process(delta):
+	get_rotation_input(delta)
+	
 	SignalBus.changed_player_rotation.emit(rotation)
 		
 	if Input.is_action_just_pressed("ui_accept"):
