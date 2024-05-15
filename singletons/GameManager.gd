013b7557192520190f3966b947e6375e866fab43
Shaw Kee <shawkee0511@gmail.com>
2024-05-15 23:43:57 +0800

    merge issue

--- singletons/GameManager.gd
+++ singletons/GameManager.gd
@@ -1,23 +1,26 @@
 extends Node
 
-enum GAME_STATE {MENU, IN_PROGRESS, PAUSED, GAME_OVER}
-var current_state: GAME_STATE
+enum GameState {IDLE, RUNNING, ENDED}
+
+var game_state
 
 func _ready():
-	current_state = GAME_STATE.IN_PROGRESS
-	pass # Replace with function body.
+	game_state = GameState.IDLE
+	SignalBus.start_game.connect(game_started)
+	SignalBus.game_state.emit(game_state)
+	SignalBus.nearest_enemy_position.connect(game_over)
+	
+func game_started(start_game:bool):
+	if start_game:
+		game_state = GameState.RUNNING
+	else:
+		game_state = GameState.IDLE
+	SignalBus.game_state.emit(game_state)
 
-func _process(delta):
+func game_over(fish_position:float):
+	var player_y_position = get_parent().get_node("Player").global_position.y
+	if fish_position >= player_y_position:
+		print("GAMEOVER!")
+
+func _process(_delta):
 	pass
-
-func enter_pause_state():
-	pass
-
-func enter_game_over_state():
-	pass
-
-func enter_menu_state():
-	pass
-
-func enter_in_progress_state():
-	pass	
