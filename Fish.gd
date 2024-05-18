9de6d824b72615da3bd38a19a407710a20ec0f2f
meepmeerp <dylgoh@gmail.com>
2024-05-18 15:56:24 +0800

    Added fish death shader anim

--- Fish.gd
+++ Fish.gd
@@ -10,15 +10,26 @@ func _ready():
 	
 	if fish_resource.has_method("fish_init"):
 		fish_resource.fish_init(self)
+	
+	$Sprite2D.material.set_shader_parameter("dissolve_value",1)
 
 func _on_area_2d_body_entered(body):
-	
+
 	if fish_resource:
 		if fish_resource.has_method("on_collision_with_body_param"):
 			fish_resource.on_collision_with_body_param(body,self)
 		else:
 			fish_resource.on_collision(body) #i'm just going to assume that the only bodies moving around are the bubble - DG
 	#make death tween here
+	var death_tween = create_tween()
+	death_tween.tween_method(set_shader_value,1.0,0.0,0.25)
+	death_tween.tween_callback(_on_fish_destroyed_tween_complete)
+
+
+func set_shader_value(value:float):
+	$Sprite2D.material.set_shader_parameter("dissolve_value",value)
+
+func _on_fish_destroyed_tween_complete():
 	queue_free()
 	SignalBus.fish_destroyed.emit(10)
 
@@ -26,7 +37,6 @@ func _on_shot_fired():
 	var tween = get_tree().create_tween()
 	tween.tween_property(self,"position",position+Vector2(0,32),0.5).set_trans(Tween.TRANS_SINE)
 	#tween.tween_property($Sprite2D, "modulate", Color.RED, 1)
-	tween.play()
 	
 func _toggle_visibility(show_flag:bool):
 	self.visible = show_flag
