f64d5d11f9ab87fa89cc360d287f6763c755b13e
AtlasWongy <atlasyjyj@gmail.com>
2024-05-20 02:09:39 +0800

    Adjust the coral effects and cuttlefish effects

--- characters/fishes/coral/Coral.gd
+++ characters/fishes/coral/Coral.gd
@@ -2,29 +2,33 @@ extends FishResource
 class_name Coral
 
 var ray_cast_arrays: Array[RayCast2D] = []
+var fish_array = []
 var fish_body: Node2D
 
 func fish_init(body):
 	fish_body = body
 	fish_body.add_to_group("coral")
-	for i in range(4):
-		match(i):
-			0:
-				ray_cast_arrays.append(generate_raycasts(Vector2(64, 0)))
-			1:
-				ray_cast_arrays.append(generate_raycasts(Vector2(0, 64)))
-			2:
-				ray_cast_arrays.append(generate_raycasts(Vector2(-64, 0)))
-			3:
-				ray_cast_arrays.append(generate_raycasts(Vector2(0, -64)))
+	for obj in fish_body.get_tree().get_nodes_in_group("Fish"):
+		if (obj.position.y <= fish_body.position.y+100 && obj.position.y >= fish_body.position.y-100 && obj.position.x <= fish_body.position.x+100 && obj.position.x >= fish_body.position.x-100):
+			obj.fish_resource.has_shield = true
+	#for i in range(4):
+		#match(i):
+			#0:
+				#ray_cast_arrays.append(generate_raycasts(Vector2(64, 0)))
+			#1:
+				#ray_cast_arrays.append(generate_raycasts(Vector2(0, 64)))
+			#2:
+				#ray_cast_arrays.append(generate_raycasts(Vector2(-64, 0)))
+			#3:
+				#ray_cast_arrays.append(generate_raycasts(Vector2(0, -64)))
 	
 func fish_physics():
-	provide_shield()
+	#provide_shield()
+	pass
 
-func on_collision(body):
-	for raycast in ray_cast_arrays:
-		#raycast.queue_free()
-		call_deferred("queue_free", raycast)
+#func on_collision(body):
+	#for raycast in ray_cast_arrays:
+		#call_deferred("free_raycast", raycast)
 
 func provide_shield():
 	for raycast in ray_cast_arrays:
@@ -46,4 +50,8 @@ func generate_raycasts(target_dir: Vector2) -> RayCast2D:
 	raycast.position = Vector2(0, 0)
 	raycast.target_position = target_dir
 	return raycast
+
+func free_raycast(raycast):
+	if is_instance_valid(raycast):
+		raycast.queue_free()
 
